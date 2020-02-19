cd80f072b9aa4dde3173de6d3b9bbad6f90bfae5
alucardthefish <sergiop18@gmail.com>
2020-02-18 23:55:25 -0500

    Adiciona archivo de especificacion de la clase Name

---
+++ 011chapter/Name.h
@@ -0,0 +1,42 @@
+// **************************************************************************** 
+// File: Name.h 
+// Author: Sergio Ortiz Paz 
+// Created: Tue Feb 18 23:34:41 2020 
+// Description: ARCHIVO DE ESPECIFICACIÓN 
+// **************************************************************************** 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum RelationType(BEFORE, SAME, AFTER);
+
+class Name
+{
+	public:
+		Name();
+		Name(
+				string firstName,
+				string middleName,
+				string lastName);
+		
+		void SetName(
+				string firstName,
+				string middleName;
+				string lastName);
+
+		void ReadName();
+		string FirstName() const;
+		string LastName() const;
+		string MiddleName() const;
+		char MiddleInitial() const;
+		RelationType ComparedTo(Name otherName) const;
+	
+	private:
+		string first;
+		string last;
+		string middle;
+
+};
+
